e02b8136563ae16f77181c2b0347f9b9162d89c2
EvanHsieh0415 <evan940415@gmail.com>
2023-03-06 13:56:14 +0800

    feat: rename

--- src/generator/list_mod.py
+++
@@ -1,7 +0,0 @@
-import tomllib
-
-with open('./src/generator/items.toml', 'rb') as file:
-    data = tomllib.load(file)
-
-with open('./src/generator/modlist.txt', 'w') as file:
-    file.writelines('\n'.join(f'- {mod}' for mod in sorted([*data.keys()]) if mod != 'minecraft'))
