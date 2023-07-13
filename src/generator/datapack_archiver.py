4314130e2b8ac6bff1018b4cfb14d76149d1ea5a
Mango <evan940415@gmail.com>
2023-07-13 18:11:24 +0800

    rename scripts

--- src/generator/datapack_archiver.py
+++ src/generator/datapack_archiver.py
@@ -1,6 +1,6 @@
 from shutil import make_archive
 
-VERSION = "1.4"
+VERSION = "1.5"
 
 make_archive(
     base_name=f"./src/generator/result/Raw-Ore-Processing-v{VERSION}",
--- src/generator/list_mods.py
+++
@@ -1,6 +0,0 @@
-from packages.local_data import *
-
-mod_ids = ["- " + i for i in items_data]
-
-with open("./data/mod_ids.md", "w") as md_file:
-    md_file.writelines("\n".join(mod_ids))
